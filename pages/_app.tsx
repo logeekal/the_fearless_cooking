f47423786bd97aafd934609ca1c2b3aa5168747d
Jatin Kathuria <jtn.kathuria@gmail.com>
2023-06-03 14:58:58 -0700

    Fix: App Re-render when searching (#42)
    * fix
    
    * Empty commit to re-trigger the  CI
    
    * final fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,15 @@ import '../styles/globals/index.scss'
 
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 import PlausibleProvider from 'next-plausible'
-import { ComponentProps, ComponentType, ReactElement, ReactNode } from 'react'
+import {
+  ComponentProps,
+  ComponentType,
+  ReactElement,
+  ReactNode,
+  useMemo,
+} from 'react'
 
 import { LayoutProps } from '../components/layout'
 
@@ -23,11 +30,23 @@ type AppPropsWithLayout<P> = AppProps<P> & {
 function MyApp({ Component, pageProps }: AppPropsWithLayout<unknown>) {
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  const CompWithPlausible = withPlausible(Component)
+  const router = useRouter()
+
+  /*
+   *
+   * Nextjs re-renders the complete app when there is
+   * a change in query string of the URL. Below
+   * memoization makes sure that App is rendered
+   * only when there is relavant state change of router
+   * */
+  const CompWithPlausible = useMemo(
+    () => withPlausible(Component),
+    [router.pathname, router.isReady, router.locale, Component]
+  )
 
   if ('layoutProps' in pageProps) {
     /* eslint-disable-next-line */
-        const { layoutProps, ...rest } = pageProps
+    const { layoutProps, ...rest } = pageProps
 
     return getLayout(<CompWithPlausible {...rest} />, layoutProps)
   }
