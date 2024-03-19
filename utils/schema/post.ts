c6e8d7ad4dbdcc555af06943be07f4cc938d1817
Jatin Kathuria <jtn.kathuria@gmail.com>
2024-03-19 15:15:35 +0100

    fix: more details in article meta

--- utils/schema/post.ts
+++ utils/schema/post.ts
@@ -1,19 +1,30 @@
 import { Post } from '../../types/wp-graphql.types'
 import { SiteMeta } from '../config'
+import { logger } from '../logger'
 
 export const genPostSchema = (post: Post) => {
+  logger.warn({ dateGmt: post.dateGmt, modifiedGmt: post.modifiedGmt })
   if (!post) return {}
 
   return {
     '@context': 'https://schema.org',
     '@type': 'Article',
     headline: post.title,
-    datePublished: `${post.dateGmt ?? Date.now()}+00:00`,
-    dateModified: `${post.modifiedGmt ?? Date.now()}+00:00`,
+    datePublished: `${post.dateGmt ?? Date.now()}Z`,
+    dateModified: `${post.modifiedGmt ?? Date.now()}Z`,
     author: {
       '@type': 'Person',
       name: SiteMeta.author.name,
+      url: SiteMeta.author.aboutLink,
     },
     image: post.featuredImage?.node?.sourceUrl,
+    publisher: {
+      '@type': 'Organization',
+      name: SiteMeta.name,
+      logo: {
+        '@type': 'ImageObject',
+        url: SiteMeta.logo,
+      },
+    },
   }
 }
